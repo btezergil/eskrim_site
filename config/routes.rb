3d39e490faef7356f4b6054caf235683148cf6d1
Berke Tezergil <btezergil@hotmail.com>
2016-09-11 20:35:40 +0300

    Sayfalara bos icerik eklendi

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/anasayfa'
+  get 'anasayfa' => 'static_pages#anasayfa'
 
-  get 'static_pages/eskrim'
+  get 'eskrim' => 'static_pages#eskrim'
 
-  get 'static_pages/antrenmanlar'
+  get 'antrenmanlar' => 'static_pages#antrenmanlar'
 
-  get 'static_pages/takimimiz'
+  get 'takimimiz' => 'static_pages#takimimiz'
 
-  get 'static_pages/duyurular'
+  get 'duyurular' => 'static_pages#duyurular'
 
-  get 'static_pages/iletisim'
+  get 'iletisim' => 'static_pages#iletisim'
 
   root 'static_pages#anasayfa'
 
